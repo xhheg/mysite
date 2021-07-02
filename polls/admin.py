3bbb6da5997187817a5bd2112661f73a8734672e
xhheg <46879687+xhheg@users.noreply.github.com>
2021-07-02 20:03:15 +0800

    初始化代码
    初始化，django测试

---
+++ polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
